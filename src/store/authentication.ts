1ba6cce965572d2dd885c8b315bb0fc2ad7dc93a
Aaron Saunders <aaronksaunders@gmail.com>
2021-01-11 23:57:51 -0500

    added android, fix undefined error

--- src/store/authentication.ts
+++ src/store/authentication.ts
@@ -5,103 +5,109 @@
 // @see https://github.com/vuejs/vuex/tree/v4.0.0-rc.1
 //
 const authentication = {
+  state() {
+    return {
+      user: null,
+      authChecked: false,
+    };
+  },
 
-    state() {
-        return {
-            user: null,
-            authChecked: false
-        };
+  // ACTIONS (asynchronous)
+  actions: {
+    /**
+     * checks to see if there was an instance of a user from
+     * the last session
+     *
+     * @param context
+     */
+    initialize(context: any) {
+      return new Promise<boolean>((resolve) => {
+        setTimeout(() => {
+          const userString = localStorage.getItem("USER");
+          const user = userString !== null ? JSON.parse(userString) : null;
+          context.commit("checkAuth", { authChecked: true, user });
+          if (user) {
+            context.commit("hasUser", { user: { email: user.email } });
+          } else {
+            context.commit("hasUser", null);
+          }
+          resolve(true);
+        }, 1000);
+      });
     },
+    /**
+     * logs in the user using the paramaeters fron the payload, email & password
+     * @param context
+     * @param payload
+     */
+    login(context: any, payload: any) {
+      return new Promise<boolean>((resolve) => {
+        setTimeout(() => {
+          if (payload.email !== "") {
+            localStorage.setItem("USER", JSON.stringify(payload));
+            if (payload.email) {
+              context.commit("hasUser", { user: { email: payload.email } });
+            } else {
+              context.commit("hasUser", null);
+            }
+            console.log(context.state.user);
+            resolve(true);
+          } else {
+            context.commit("clearUser", {});
+            resolve(false);
+          }
+        }, 1000);
+      });
+    },
+    /**
+     * logout the user, clear the information from local storage
+     * @param context
+     */
+    logout(context: any) {
+      return new Promise<boolean>((resolve) => {
+        setTimeout(() => {
+          context.commit("clearUser", {});
+          localStorage.removeItem("USER");
+          resolve(true);
+        }, 1000);
+      });
+    },
+  },
 
-    // ACTIONS (asynchronous)
-    actions: {
-        /**
-         * checks to see if there was an instance of a user from 
-         * the last session
-         * 
-         * @param context 
-         */
-        initialize(context: any) {
-            return new Promise<boolean>((resolve) => {
-                setTimeout(() => {
-                    const userString = localStorage.getItem("USER")
-                    const user = userString !== null ? JSON.parse(userString) : null;
-                    context.commit("checkAuth", { authChecked: true, user });
-                    context.commit("hasUser", { ...user });
-                    resolve(true);
-                }, 1000);
-            });
-        },
-        /**
-         * logs in the user using the paramaeters fron the payload, email & password
-         * @param context 
-         * @param payload 
-         */
-        login(context: any, payload: any) {
-            return new Promise<boolean>((resolve) => {
-                setTimeout(() => {
-                    if (payload.email !== "") {
-                        localStorage.setItem("USER", JSON.stringify(payload))
-                        context.commit("hasUser", { ...payload });
-                        console.log(context.state.user);
-                        resolve(true);
-                    } else {
-                        context.commit("clearUser", {});
-                        resolve(false);
-                    }
-                }, 1000);
-            });
-        },
-        /**
-         * logout the user, clear the information from local storage
-         * @param context 
-         */
-        logout(context: any) {
-            return new Promise<boolean>((resolve) => {
-                setTimeout(() => {
-                    context.commit("clearUser", {});
-                    localStorage.removeItem("USER");
-                    resolve(true);
-                }, 1000);
-            });
-
-        },
+  // MUTATIONS ( set the state )
+  mutations: {
+    hasUser(state: any, payload: { user: { email: string } }) {
+      state.user = payload?.user || null;
+      console.log(state);
     },
-
-    // MUTATIONS ( set the state )
-    mutations: {
-        hasUser(state: any, payload: any) {
-            state.user = { ...payload };
-            console.log(state)
-        },
-        clearUser(state: any) {
-            state.user = null;
-            console.log(state)
-        },
-        checkAuth(state: any, payload: any) {
-            debugger;
-            state.user = payload.user;
-            state.authChecked = payload.authChecked;
-            console.log(state)
-        },
+    clearUser(state: any) {
+      state.user = null;
+      console.log(state);
     },
-    getters: {
-        /**
-         * returns true or false based on if a user exists or not
-         * @param state 
-         */
-        isLoggedIn(state: any) {
-            return state.user !== null
-        },
-        /**
-         * returns the current user
-         * @param state 
-         */
-        currentUser(state: any) {
-            return state.user;
-        }
+    checkAuth(state: any, payload: any) {
+      debugger;
+      state.user = payload.user;
+      state.authChecked = payload.authChecked;
+      console.log(state);
     },
-    namespaced: true
+  },
+  getters: {
+    /**
+     * returns true or false based on if a user exists or not
+     * @param state
+     */
+    isLoggedIn(state: any) {
+      return state.user !== null;
+    },
+    /**
+     * returns the current user
+     * @param state
+     */
+    currentUser(state: any) {
+      return state.user;
+    },
+  },
+  namespaced: true,
 };
 
 export default authentication;
